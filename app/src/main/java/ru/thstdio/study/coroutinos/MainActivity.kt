649560328c4002ce5f5800432cd25b0f65037db4
tosh17 <tosh17@list.ru>
2019-08-04 14:43:43 +0300

    add count core cpu

--- app/src/main/java/ru/thstdio/study/coroutinos/MainActivity.kt
+++ app/src/main/java/ru/thstdio/study/coroutinos/MainActivity.kt
@@ -1,74 +1,48 @@
 package ru.thstdio.study.coroutinos
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.channels.SendChannel
-import kotlinx.coroutines.channels.actor
-import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import ru.thstdio.study.coroutinos.example1.Example1Activity
 import ru.thstdio.study.coroutinos.hardwork.BigData
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var printChannel: SendChannel<Float>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startExample(1)
         setContentView(R.layout.activity_main)
-
-        initChanel()
-        btn1.setOnClickListener { ckickBtn1() }
-
-    }
-
-    private fun initChanel() = runBlocking {
-        launch {
-            printChannel = actor {
-                // Переменная channel представляет канал внутри функции
-                for (f in channel) {
-                    println("actor: $f ${Thread.currentThread().name}")
-                    Log.d("Croutine", f.toString())
-                    showProgress(1, f)
-                }
-            }
-        }
-    }
-
-
-    var currentProgress = 0
-    fun showProgress(stream: Int, progress: Float) {
-        when (stream) {
-            1 -> {
-                if (progress.toInt() > currentProgress) {
-                    currentProgress = progress.toInt()
-                    progressBar1.setProgress(progress.toInt())
-                    Log.d("setProgress", progress.toInt().toString())
-                }
-
-            }
-        }
-    }
-
-
-    fun ckickBtn1() = runBlocking<Unit> {
-        text_out_stream.text = ""
-        showProgress(1, 0f)
-        produce<String>(Dispatchers.IO) {
-
+        btn_test1_start.setOnClickListener{
+            text_out_stream.text=""
             val factor = BigData()
-            val j = 10000
+            val j: Int
+            j = 700
             for (i in 1..j) {
                 factor.data = factor.multiply(i)
-                printChannel.send((100 * i).toFloat() / j)
+                text_out_stream.text="Current position ${100*i/j}%"
             }
-
-            //  text_out_stream.text="Факториал  $j!=+${factor.data.length}"
+            text_out_stream.text="Факториал  $j!=+${factor.data.length}"
         }
+    }
+
+    private fun startExample(number: Int) {
+        val next= Intent(this,
+           when(number){
+               else -> Example1Activity::class.java
+           } )
+        startActivity(next)
+        finish()
+
+    }
+
+    fun showProgress(stream:Int,progress:Float)=runBlocking<Unit> {
+
+        when(stream){
+            1-> progressBar1.setProgress(progress.toInt())
+        }
+
     }
 }
--- app/src/main/java/ru/thstdio/study/coroutinos/example1/Example1Activity.kt
+++ app/src/main/java/ru/thstdio/study/coroutinos/example1/Example1Activity.kt
@@ -6,11 +6,18 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_example1.*
 import kotlinx.coroutines.*
 import ru.thstdio.study.coroutinos.R
 import ru.thstdio.study.coroutinos.hardwork.BigData
+import java.io.File
+import java.io.FileFilter
+import java.util.regex.Pattern
+
 
 class Example1Activity : AppCompatActivity(), CoroutineScope by MainScope() {
     override fun onDestroy() {
@@ -19,29 +26,54 @@ class Example1Activity : AppCompatActivity(), CoroutineScope by MainScope() {
     }
 
     val startHardWork = 1
-    val finishHardWork = 1500
+    var finishHardWork = 1500
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_example1)
+        initSpiner(getNumCores())
+        initFinish()
         btn_test1_start.setOnClickListener { startProgress() }
         btn_example1_hard_work.setOnClickListener {
-            textView_hard_work_result.text = ""
-            val data = hardWork(startHardWork, finishHardWork).countChar()
-            textView_hard_work_result.text = data
+            val startTime = System.currentTimeMillis()
+            val data = hardWork(startHardWork, finishHardWork)
+            textView_hard_work_result.append(generateResult(data, startTime).end())
         }
         btn_example1_coroutine_hard_work.setOnClickListener {
-            textView_hard_work_result.text = ""
             GlobalScope.launch {
-                val data =hardWorkCoroutinos(startHardWork, finishHardWork).countChar()
-                textView_hard_work_result.text = data
+                val startTime = System.currentTimeMillis()
+                val data = hardWorkCoroutinos(startHardWork, finishHardWork)
+                textView_hard_work_result.append(generateResult(data, startTime).end())
             }
 
         }
-        btn_example1_coroutine_hard_work_4_stream.setOnClickListener {
-            textView_hard_work_result.text = ""
-            hardworkTo4Stream()
-        }
+        btn_example1_coroutine_hard_work_x_stream.setOnClickListener { hardworkToXCore() }
         anim()
+    }
+
+    private fun initFinish() {
+        editText_ex1_factorial.setText(finishHardWork.toString())
+        editText_ex1_factorial.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (p0.toString() == "") finishHardWork = 1
+                else p0.let { finishHardWork = it.toString().toInt() }
+            }
+        })
+    }
+
+    private fun initSpiner(core: Int) {
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, (1..4 * core).toMutableList())
+        // Определяем разметку для использования при выборе элемента
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner_exm1.adapter = adapter
+
     }
 
     private fun anim() {
@@ -64,9 +96,9 @@ class Example1Activity : AppCompatActivity(), CoroutineScope by MainScope() {
         btn_test1_stop.setOnClickListener { job.cancel() }
     }
 
-    private fun hardWork(start: Int, finish: Int): String {
+    private fun hardWork(start: Int, finish: Int, core: Int = 1): String {
         val factor = BigData()
-        for (i in start..finish) {
+        for (i in start..finish step core) {
             factor.data = factor.multiply(i)
         }
         return factor.data
@@ -81,45 +113,96 @@ class Example1Activity : AppCompatActivity(), CoroutineScope by MainScope() {
             val data = deferred.await()
             return@withContext data
         }
-
     }
 
-    private fun hardworkTo4Stream() {
-        val step = (finishHardWork / 4)
-        var result1 = "0"
-        var result2 = "0"
-        var result3 = "0"
-        var result4 = "0"
-        val job1 = GlobalScope.launch {
-            result1 = hardWorkCoroutinos(startHardWork, step - 1)
+    suspend fun hardWorkCoroutinos(start: Int, finish: Int, core: Int=1): String {
+        val deferred = async(Dispatchers.IO) {
+            hardWork(start, finish, core)
         }
-        val job2 = GlobalScope.launch {
-            result2 = hardWorkCoroutinos(step, 2 * step - 1)
+        return withContext(Dispatchers.Main) {
+            val data = deferred.await()
+            return@withContext data
         }
-        val job3 = GlobalScope.launch {
-            result3 = hardWorkCoroutinos(step * 2, 3 * step - 1)
-        }
-        val job4 = GlobalScope.launch {
-            result4 = hardWorkCoroutinos(3 * step, finishHardWork)
+    }
+
+    private fun hardworkToXCore() {
+        printStart()
+        val coreNumber = spinner_exm1.selectedItemPosition + 1
+        val startTime = System.currentTimeMillis()
+        val result: MutableList<String> = ArrayList()
+        val job: MutableList<Job> = ArrayList()
+
+        for (core in 1..coreNumber) {
+            job.add(GlobalScope.launch {
+                result.add(hardWorkCoroutinos(core, finishHardWork, coreNumber))
+            })
         }
         GlobalScope.launch {
-            job1.join()
-            job2.join()
-            job3.join()
-            job4.join()
-            val factor = BigData(result1)
-            factor.data = factor.multiply(result2)
-            factor.data = factor.multiply(result3)
-            factor.data = factor.multiply(result4)
-            textView_hard_work_result.text = factor.data.countChar()
+            for (core in 0 until coreNumber) job[core].join()
+
+            val factor = BigData(result[0])
+            for (core in 1 until coreNumber) factor.data = factor.multiply(result[core])
+
+            val str = generateResult(factor.data, startTime, coreNumber)
+            textView_hard_work_result.append(str.end())
         }
     }
+
+
+    private fun generateResult(data: String): String = "${finishHardWork}!.lenght = ${data.countChar()}  "
+    private fun generateResult(data: String, startTime: Long): String =
+        generateResult(data) + "calc time = ${startTime.getDeltaFromCurrentTime()}"
+
+    private fun generateResult(data: String, startTime: Long, coreNumber: Int): String =
+        generateResult(data, startTime) + " core=${coreNumber}"
 
     private fun printToast(msg: String) {
         Toast.makeText(baseContext, msg, Toast.LENGTH_LONG).show()
     }
+
+    private fun printStart() {
+        textView_hard_work_result.append("--> ")
+    }
+
+    private fun getNumCores(): Int {
+        //Private Class to display only CPU devices in the directory listing
+        class CpuFilter : FileFilter {
+            override fun accept(pathname: File): Boolean {
+                //Check if filename is "cpu", followed by one or more digits
+                return if (Pattern.matches("cpu[0-9]+", pathname.getName())) {
+                    true
+                } else false
+            }
+        }
+
+        try {
+            //Get directory containing CPU info
+            val dir = File("/sys/devices/system/cpu/")
+            //Filter to only list the devices we care about
+            val files = dir.listFiles(CpuFilter())
+            //Return the number of cores (virtual CPU devices)
+            return files.size
+        } catch (e: Exception) {
+            //Default to return 1 core
+            return 1
+        }
+
+    }
+}
+
+private fun Long.getDeltaFromCurrentTime(): String {
+    val currentTime = (System.currentTimeMillis() - this) / 1000
+    val minute = currentTime / 60
+    val secunde = currentTime % 60
+
+    return java.lang.String.format("%02d:%02d", minute, secunde)
+
 }
 
 private fun String.countChar(): String {
     return this.length.toString()
+}
+
+private fun String.end(): String {
+    return this + "\n"
 }
